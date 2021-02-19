79234458f5a4ef5d186ca39b371ae93e40bf6d25
deisaacs98 <deisaacs98@gmail.com>
2021-02-19 13:15:23 -0600

    Changed customer and employee models and views.

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -17,7 +17,6 @@ namespace TrashCollector.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
 
         public DayOfWeek RegularPickupDay { get; set; }
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -15,9 +15,7 @@ namespace TrashCollector.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Address { get; set; }
         public int ZipCode { get; set; }
-        public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
 
         [ForeignKey("IdentityUser")]
